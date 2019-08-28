28f3d4c4aa95bcd344156b5b09c16d92b83b1e24
peter <peteruabeshi>
2019-08-28 14:43:50 +0100

    adding files

--- MaximumFlow/Program.cs
+++ MaximumFlow/Program.cs
@@ -10,6 +10,29 @@ namespace MaximumFlow
     {
         static void Main(string[] args)
         {
+            Graph graph = new Graph(6);
+            //graph.AddEdge(0,1,10);
+            //graph.AddEdge(0,2,8);
+            //graph.AddEdge(1,2,5);
+            //graph.AddEdge(1,3,5);
+            //graph.AddEdge(2,1,4);
+            //graph.AddEdge(2,4,10);
+            //graph.AddEdge(3,2,7);
+            //graph.AddEdge(3,4,6);
+            //graph.AddEdge(3,5,3);
+            //graph.AddEdge(4,3,10);
+            //graph.AddEdge(4,5,14);
+            graph.AddEdge(0,1,2);
+            graph.AddEdge(0,2,8);
+            graph.AddEdge(0,3,3);
+            graph.AddEdge(1,2,7);
+            graph.AddEdge(2,3,2);
+            graph.AddEdge(1,4,2);
+            graph.AddEdge(3,4,5);
+
+            int maxFlow = graph.FindMaximumFlow(0, 4);
+            Console.WriteLine(maxFlow);
+            Console.ReadKey();
         }
     }
 }
